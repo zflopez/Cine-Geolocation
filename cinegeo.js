93410cc9d41fb7de8c70d4a53c81beb56fbd144d
adridlv <adriandlv53@gmail.com>
2016-06-27 16:38:41 +0200

    terminado

--- cinegeo.js
+++ cinegeo.js
@@ -1,6 +1,15 @@
 var map;
+var userLat;
+var userLon;
+var markers = [];
 
-function getData() {
+function mapButtonSubmit() {
+	var cinema = getData();
+	saveLocalStorage(cinema);	
+	updateMap();
+}
+
+function getData(){
 	var cinemaName = $("input[name='cinema']").val();
 	var latName = $("input[name='latitude']").val();
 	var lonName = $("input[name='longitude']").val();
@@ -10,14 +19,19 @@ function getData() {
 	cinema["latitude"] = latName;
 	cinema["longitude"] = lonName;
 
-	toLocalStorage();	
+	return cinema;
 }
 
-function toLocalStorage() {
+function saveLocalStorage(cinema) {
 	var storedCinemas = JSON.parse(window.localStorage.getItem('cinemas')) || {};
 	storedCinemas[Object.keys(storedCinemas).length + 1] = cinema;
-
 	window.localStorage.setItem("cinemas", JSON.stringify(storedCinemas));
+}
+
+function setMarkersOnMap(map) {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+	}
 }
 
 function getLocation() {
@@ -31,33 +45,39 @@ function getLocation() {
 }
 
 function onLocation(position){
+	userLat = position.coords.latitude;
+	userLon = position.coords.longitude;
 	map = new google.maps.Map(document.getElementById('map'),{
 		zoom:10,
-		center: new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
+		center: new google.maps.LatLng(userLat,userLon),
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
 
+	updateMap();
+}
+
+function getSortedArray(){
 	var storedCinemas = JSON.parse(window.localStorage.getItem('cinemas')) || {};
 	var nearestCinema;
 	var nearestCinemaDistance = Infinity;
 
 	var locations = [
-	['You', position.coords.latitude, position.coords.longitude, 0]
+	['You', userLat, userLon, 0]
 	];
 
 	for(cinemaIndex in storedCinemas){
 		console.log(storedCinemas[cinemaIndex].name);
-		var distanceAux = distance(position.coords.latitude, position.coords.longitude, storedCinemas[cinemaIndex].latitude, storedCinemas[cinemaIndex].longitude, "km");	
+		var distanceAux = distance(userLat, userLon, storedCinemas[cinemaIndex].latitude, 
+			storedCinemas[cinemaIndex].longitude, "km");	
 
-		var cinema = [storedCinemas[cinemaIndex].name, storedCinemas[cinemaIndex].latitude, storedCinemas[cinemaIndex].longitude, distanceAux];
+		var cinema = [storedCinemas[cinemaIndex].name, storedCinemas[cinemaIndex].latitude, 
+		storedCinemas[cinemaIndex].longitude, distanceAux];
 		locations.push(cinema);
 
 		if(distanceAux < nearestCinemaDistance){
 			nearestCinema = storedCinemas[cinemaIndex];
 		}
 	}
-
-	getMarkers(locations);
 
 	var arrayAux = locations.sort(function(a, b) {
 		if (a[3] > b[3])
@@ -69,25 +89,48 @@ function onLocation(position){
 		return 0;
 	});
 
-	console.log(arrayAux);
+	return arrayAux;
+}
 
-	var i;
-	var htmlList = "<li>" + arrayAux[i] + "</li>";
-	$('.locationList').append(htmlList);
+function updateMap(){
+	var arrayAux = getSortedArray();
 
+	$('.locationList').empty(); 
+
+	for (var i = 0; i < arrayAux.length; i++) {
+		var htmlList = "<li><strong>Location: </strong>" + arrayAux[i][0] + "<strong> Latitude: </strong>" 
+		+ arrayAux[i][1]+ "<strong> Longitude: </strong>" + arrayAux[i][2] + "<strong> Distance: </strong>" 
+		+ arrayAux[i][3] + "</li>";
+		$('.locationList').append(htmlList);	
+	}
+
+	getMarkers(arrayAux);
 }
 
 function getMarkers(array) {
 	var infowindow = new google.maps.InfoWindow();
 	var marker, i;
+	setMarkersOnMap(null); //resets the map markers
+	markers = []; //array that stores the map markers
 
 	for (i = 0; i < array.length; i++) {
-		console.log(array[i][1]);
+		var icon = undefined;
+
+		if (i === 0) { //if 0 its home
+			icon = 'https://maps.google.com/mapfiles/kml/shapes/' + 'ranger_station.png';
+		}
+
+		if (i === 1) { //if 1 its the closest cinema
+			icon = 'https://maps.google.com/mapfiles/kml/shapes/' + 'movies.png';
+		}
+
 		marker = new google.maps.Marker({
 			position: new google.maps.LatLng(array[i][1], array[i][2]),
 			map: map,
-			icon: 'https://maps.google.com/mapfiles/kml/shapes/' + 'schools_maps.png'
+			icon: icon
 		});
+
+		markers.push(marker); //stores the marker into the array
 
 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
 			return function() {
@@ -120,7 +163,7 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 $(document).ready(function() {
 	$('.buttonSubmit').click(function(e){
 		e.preventDefault();
-		getData();
+		mapButtonSubmit();
 	});
 
 	if ("geolocation" in navigator) {
@@ -129,6 +172,3 @@ $(document).ready(function() {
 		alert("Geolocation is not available")
 	}
 });
-
-
-//!= ""
